81f219d9ba1b51b222be14d9f4e8ccaeff98f0f7
akinola Olanrewaju <akinola.lanrewaju@yahoo.com>
2023-06-19 15:03:58 -0700

    declare and export nextISSlocation function

--- iss_promised.js
+++ iss_promised.js
@@ -1,9 +1,28 @@
 const request = require("request-promise-native");
 
-const fetchMyIp = function () {
+const fetchMyIp = function() {
   return request("https://api.ipify.org/?format=json");
 };
 
+const fetchCoordsByIP = function(body) {
+  const ip = JSON.parse(body).ip;
+  return request(`http://ipwho.is/${ip}`);
+};
+const fetchISSFlyOverTimes = function(body) {
+  const { latitude, longitude } = JSON.parse(body);
+  return request(`https://iss-flyover.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`);
+};
+
+const nextISStimesForMyLocation = function() {
+  return fetchMyIp()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then((data) => {
+      const { response } = JSON.parse(data);
+      return response;
+    });
+};
+
 module.exports = {
-  fetchMyIp
+  nextISStimesForMyLocation
 };
