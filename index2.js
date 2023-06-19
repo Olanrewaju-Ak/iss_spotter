e716973f06c422ecdf648237b7cb5cb93a8aecb5
akinola Olanrewaju <akinola.lanrewaju@yahoo.com>
2023-06-19 15:17:06 -0700

    add error handling catch

--- index2.js
+++ index2.js
@@ -1,7 +1,11 @@
 const { nextISStimesForMyLocation } = require("./iss_promised");
 
-nextISStimesForMyLocation().then((data) => {
-  for (const item of data) {
-    console.log("Next pass at " + new Date(item.risetime), "for " + item.duration + " seconds");
-  }
-});
+nextISStimesForMyLocation()
+  .then((data) => {
+    for (const item of data) {
+      console.log("Next pass at " + new Date(item.risetime), "for " + item.duration + " seconds");
+    }
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
